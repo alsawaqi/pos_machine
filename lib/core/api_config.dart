180e762227aff3a22e039f6727f88eef702dd0a4
alsawaqi <abdallah644@hotmail.com>
2026-06-22 22:25:30 +0400

    s

--- lib/core/api_config.dart
+++ lib/core/api_config.dart
@@ -10,7 +10,7 @@ class ApiConfig {
 
   static const String baseUrl = String.fromEnvironment(
     'POS_API_BASE_URL',
-    defaultValue: 'http://localhost:8088/api/v1',
+    defaultValue: 'https://posapi.mithqal.net/api/v1',
   );
 
   static const Duration connectTimeout = Duration(seconds: 10);
